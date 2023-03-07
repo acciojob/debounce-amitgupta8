287883b30dde028bcda0230a7c5d0b6745178b6a
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-07 17:52:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function debounce(callback, delay, immediate = false) {
-  
-  }
-  
-  module.exports = debounce;
+  let timeoutId;
+
+  return function debounced(...args) {
+    const context = this;
+
+    function delayedCallback() {
+      if (!immediate) {
+        callback.apply(context, args);
+      }
+      timeoutId = null;
+    }
+
+    const shouldCallImmediately = immediate && timeoutId === undefined;
+
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(delayedCallback, delay);
+
+    if (shouldCallImmediately) {
+      callback.apply(context, args);
+    }
+  };
+}
